bdf56dba6dfa0968bafd5768dac67e2d8760ab19
osazeimohe <osazeimohe@yahoo.com>
2023-02-27 23:18:52 -0500

    stretch features

--- app/src/main/java/com/example/codepathmail/MainActivity.kt
+++ app/src/main/java/com/example/codepathmail/MainActivity.kt
@@ -2,13 +2,15 @@ package com.example.codepathmail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-    lateinit var emails: List<Email>
+
 class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var emails: List<Email>
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -18,6 +20,12 @@ class MainActivity : AppCompatActivity() {
         EmailsRv.adapter = emailAdapter
 
         EmailsRv.layoutManager = LinearLayoutManager(this)
+        findViewById<Button>(R.id.LoadMoreBtn).setOnClickListener {
+            // Fetch next 5 emails and display in RecyclerView
+            emails.addAll(EmailFetcher.getNext5Emails())
+
+            emailAdapter.notifyDataSetChanged()
+        }
 
     }
 }
